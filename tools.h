6915609994527e0688db49efef4fe07200f63c02
TheBuzzSaw <thebuzzsaw@gmail.com>
2013-03-09 14:10:35 -0700

    improved tools

--- tools.h
+++ tools.h
@@ -1,10 +1,18 @@
 #ifndef TOOLS_H
 #define TOOLS_H
 
+#include <cstring>
+
 template<typename T>
-T convert(void* data)
+T convert(const void* data)
 {
-    return *reinterpret_cast<T*>(data);
+    return *reinterpret_cast<const T*>(data);
+}
+
+template<typename T>
+void write(void* data, T value)
+{
+    memcpy(data, &value, sizeof(T));
 }
 
 #endif // TOOLS_H
